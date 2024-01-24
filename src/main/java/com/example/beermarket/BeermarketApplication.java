02b2116a201ced37a27120f1e57375d83dc65345
SergeiMuromtsev <k29393@yandex.ru>
2024-01-25 01:20:09 +0500

    initial commit

---
+++ src/main/java/com/example/beermarket/BeermarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.beermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beermarket/BeermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
